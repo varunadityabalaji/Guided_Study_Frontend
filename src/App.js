cc3e9690c785a76a003c394957a90e5b1a753ca2
Varun Aditya Balaji <varun@Varuns-MacBook-Pro.local>
2022-07-01 16:08:52 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
       </header>
     </div>
   );
